6a3e2d6e8bd4f47e08fe40401be6e00aedc0c79a
Allafou <zakariaalla6@gmail.com>
2024-12-06 16:25:19 +0100

    version1.0

---
+++ src/main/java/com/workify/agence_recrut/AgenceRecrutApplication.java
@@ -0,0 +1,13 @@
+package com.workify.agence_recrut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenceRecrutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenceRecrutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workify/agence_recrut/AgenceRecrutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workify.agence_recrut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenceRecrutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
